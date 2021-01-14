e64af9a37571e8f9951e89dbd42d9f5dfca61312
Wren Liang <wren.liang@gmail.com>
2021-01-13 23:24:50 -0700

    removed unnecessary div for background color

--- src/Components/FinishedScreen/FinishedScreen.js
+++ src/Components/FinishedScreen/FinishedScreen.js
@@ -28,7 +28,6 @@ class FinishedScreen extends React.Component {
         if (this.props.showFinishedScreen === true) {
             return (
                 <div className="FinishedScreen">
-                    <div className="FinishedScreenBackground"></div>
                     <h1 className="FinishedScreenTitle">Your Nominees for the Shoppies Movie Awards are...</h1>
                     <div className="FinishedScreenMovieList">
                         {this.nominatedMovies()}
